88ddac91b277ffdd016880cab6fb142f3c608393
vishnu <vishnu@kapow.games>
2018-08-21 19:36:04 +0530

    Controller: Add class for keeping static configuration

---
+++ src/main/java/com/intercom/interview/test/Config.java
@@ -0,0 +1,8 @@
+package com.intercom.interview.test;
+
+import com.intercom.interview.test.models.Location;
+
+public class Config {
+    public static final Location OFFICE_LOCATION = new Location(53.339428, -6.257664);
+    public static final long RADIUS_IN_KMS = 100;
+}
