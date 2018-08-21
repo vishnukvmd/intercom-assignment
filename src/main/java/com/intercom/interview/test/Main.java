318731c0aaae874dfefe7cb92207f9117c4bde89
vishnu <vishnu@kapow.games>
2018-08-21 19:49:53 +0530

    Update the Main class

--- src/main/java/com/intercom/interview/test/Main.java
+++ src/main/java/com/intercom/interview/test/Main.java
@@ -1,7 +1,17 @@
 package com.intercom.interview.test;
 
+import com.intercom.interview.test.controllers.CustomerRecordsController;
+import com.intercom.interview.test.io.FileReader;
+import com.intercom.interview.test.io.PrettyPrinter;
+import com.intercom.interview.test.models.Customer;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws IOException {
+        List<String> input = FileReader.readFile(args[0]);
+        List<Customer> invitees = new CustomerRecordsController().getRecords(input);
+        PrettyPrinter.printCustomers(invitees);
     }
 }
