c6519222259bbbdcaae60817be391571ff82e752
vishnu <vishnu@kapow.games>
2018-08-21 20:10:35 +0530

    Tests: Add test to execute a sample run for the given input data-set

---
+++ src/test/java/com/intercom/interview/tests/MainTest.java
@@ -0,0 +1,15 @@
+package com.intercom.interview.tests;
+
+import com.intercom.interview.test.Main;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class MainTest {
+    @Test
+    public void test() throws IOException {
+        String[] args = {"src/main/resources/customers.txt"};
+        Main.main(args);
+    }
+}
