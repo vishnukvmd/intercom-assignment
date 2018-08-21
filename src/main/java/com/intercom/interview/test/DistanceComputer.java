d2c0cbbad3f8787e18543bab7b9fefd71197bd52
vishnu <vishnu@kapow.games>
2018-08-21 18:26:41 +0530

    Controller: Create DistanceComputer borrowing formulas from the "Great Circle Distance"

---
+++ src/main/java/com/intercom/interview/test/DistanceComputer.java
@@ -0,0 +1,30 @@
+package com.intercom.interview.test;
+
+import com.intercom.interview.test.models.Location;
+
+import static java.lang.Math.acos;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
+
+public class DistanceComputer {
+    private static final int RADIUS_OF_EARTH_IN_KILOMETERS = 6371;
+
+    /**
+     * @see <a href="https://en.wikipedia.org/wiki/Great-circle_distance#Formulas">Great-circle distance
+    </a>
+     */
+    public static double computeDistance(Location firstLocation, Location secondLocation) {
+        double phi1 = toRadians(firstLocation.latitude);
+        double lambda1 = toRadians(firstLocation.longitude);
+        double phi2 = toRadians(secondLocation.latitude);
+        double lambda2 = toRadians(secondLocation.longitude);
+
+        double deltaLambda = Math.abs(lambda1 - lambda2);
+
+        double deltaSigma = acos((sin(phi1) * sin(phi2)) +
+                (cos(phi1) * cos(phi2) * cos(deltaLambda)));
+
+        return deltaSigma * RADIUS_OF_EARTH_IN_KILOMETERS;
+    }
+}
---
+++ src/main/java/com/intercom/interview/test/models/Location.java
@@ -0,0 +1,11 @@
+package com.intercom.interview.test.models;
+
+public class Location {
+    public final double latitude;
+    public final double longitude;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
